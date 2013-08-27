e4e66543435884b1a8c4e25f549758e2b3e45f07
eldar <eldar.ablaev@gmail.com>
2013-08-27 22:46:24 +0300

    addPlayer method in Game, Player constructor

--- src/loc/eldar/game/Player.java
+++ src/loc/eldar/game/Player.java
@@ -1,5 +1,23 @@
 package loc.eldar.game;
 
 public class Player {
+	
+	/** идентификатор игрока */
+	private final int id;
+	/** имя игрока */
+	private final String name;
+	/** фигура игрока */
+	private final Piece piece;
 
+	/**
+	 * Конструктор Player
+	 * @param id - идентификатор игрока
+	 * @param name - имя игрока
+	 * @param piece - фигура игрока
+	 */
+	Player(int id, String name, Piece piece) {
+		this.id = id;
+		this.name = name;
+		this.piece = piece;
+	}
 }
